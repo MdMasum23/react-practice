69f1c6cec630e1c3f12991741657dd89987645a1
MdMasum23 <mdmasum67172019@gmail.com>
2025-09-13 00:39:41 +0600

    Data Fetch with useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Use_state from './components/use_state.jsx'
+import Use_effect from './components/Use_effect.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Use_state/>
+    <Use_effect/>
   </StrictMode>,
 )
